1e5af8471f016083fc7fc7a50fc97b1c3b7b1856
Mohamed <mohamed.ebarhmatin@yahoo.fr>
2023-09-27 22:58:15 +0200

    q10 done

---
+++ src/app/cocktail-list/cocktail-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Cocktail } from '../models/cocktail.model';
+import { CocktailService } from '../cocktail.service';
+
+@Component({
+  selector: 'app-cocktail-list',
+  templateUrl: './cocktail-list.component.html',
+  styleUrls: ['./cocktail-list.component.css']
+})
+
+export class CocktailListComponent implements OnInit{
+
+  cocktails : Cocktail[] = [];
+
+  // injection du service dans le constructeur
+  constructor(private cocktailService: CocktailService ){}
+
+  ngOnInit(): void {
+    this.cocktails = this.cocktailService.getCocktails() 
+  }
+}
